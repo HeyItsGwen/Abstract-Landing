a71c3a68f5d0dd5f548ed6714c29c95edadaab4d
Gwen <itsgwenhey@gmail.com>
2020-10-21 05:19:36 -0500

    Updates

---
+++ precache-manifest.92ee3e2a1a25a0689a81800fd5c948d1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ef4b4bd5650a1466025fd719ab53b3",
+    "url": "/Abstract-Landing/index.html"
+  },
+  {
+    "revision": "190337570620bd2ec72d",
+    "url": "/Abstract-Landing/static/css/main.58600542.chunk.css"
+  },
+  {
+    "revision": "dc4c597ac85fc343f648",
+    "url": "/Abstract-Landing/static/js/2.1e6aec20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Abstract-Landing/static/js/2.1e6aec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190337570620bd2ec72d",
+    "url": "/Abstract-Landing/static/js/main.24554a1a.chunk.js"
+  },
+  {
+    "revision": "19a1d08755c79b0b6311",
+    "url": "/Abstract-Landing/static/js/runtime-main.1eb122c5.js"
+  },
+  {
+    "revision": "da5f036246d4f71bbabd74b07e516ece",
+    "url": "/Abstract-Landing/static/media/bubbles.da5f0362.jpg"
+  },
+  {
+    "revision": "f8cbd47750369deb23c9d28b008678f1",
+    "url": "/Abstract-Landing/static/media/bugs.f8cbd477.jpg"
+  },
+  {
+    "revision": "0bffd9cb34228a3debe6dc6851e42142",
+    "url": "/Abstract-Landing/static/media/coralbg.0bffd9cb.png"
+  },
+  {
+    "revision": "1c5911b919feaebdabe06a9cdb3a021d",
+    "url": "/Abstract-Landing/static/media/cyanbg.1c5911b9.png"
+  },
+  {
+    "revision": "8b6d295d79e6a250533160f891773129",
+    "url": "/Abstract-Landing/static/media/deedee.8b6d295d.jpg"
+  },
+  {
+    "revision": "908a18f748feeee4634d9d642f5b4545",
+    "url": "/Abstract-Landing/static/media/finn.908a18f7.jpg"
+  },
+  {
+    "revision": "b8e50f9206777da04fb961c87c81d059",
+    "url": "/Abstract-Landing/static/media/glimmer.b8e50f92.jpg"
+  },
+  {
+    "revision": "dc265107281662a2558d2594645a22a9",
+    "url": "/Abstract-Landing/static/media/jake.dc265107.jpg"
+  },
+  {
+    "revision": "18cd5e1c41a95e3c536f3d02573e98ea",
+    "url": "/Abstract-Landing/static/media/magentabg.18cd5e1c.png"
+  },
+  {
+    "revision": "f3fcf76f43b06b088a262a8eefaf6b39",
+    "url": "/Abstract-Landing/static/media/plank.f3fcf76f.png"
+  },
+  {
+    "revision": "09b6d93bb9494391dac565f8b393cb36",
+    "url": "/Abstract-Landing/static/media/whitepink.09b6d93b.png"
+  }
+]);
